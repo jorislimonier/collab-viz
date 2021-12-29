5ad416fa58f9664937a6ce163ff0098cfbbc21b5
jorislimonier <joris.limonier@hotmail.fr>
2021-12-29 20:28:08 +0100

    filter for genre in nodes that have something to do with albums

--- joris/sankey-data.js
+++ joris/sankey-data.js
@@ -1,26 +1,50 @@
 const fs = require("fs");
 
 var data = require("./sankey-genre.json");
+var nodes = data["nodes"];
 var links = data["links"];
 
-let genres = ["Acid Rock", "Acid Jazz", "Acid House", "Acid Techno", "Pop"];
+var genres = ["Acid Rock", "Acid Jazz", "Acid House", "Acid Techno", "Pop"];
 
-var filteredGenres = links.filter(({ genre }) => genres.includes(genre));
+const nodesToFilter = nodes
+  .filter(({ name }) => name.includes("album"))
+  .map((nodeGroup) => nodeGroup.node);
 
-var rock = links
-  .filter(({ genre }) => genres.includes(genre))
-  .reduce((sum, link) => sum + link["value"], 0);
+console.log(nodesToFilter);
 
+var filteredGenres = links;
+// var filteredGenres = filteredGenres.filter(({ genre }) => genres.includes(genre));
+
+var filteredGenres = links.slice(0, 99).map((row) => {
+  if (
+    (nodesToFilter.includes(row.source) ||
+      nodesToFilter.includes(row.target)) &&
+    !genres.includes(row.genre)
+  ) {
+    row.value = 0;
+    // console.log(row);
+  } else {
+    // console.log(row);
+  }
+  delete row.genre;
+  return row;
+});
+
+console.log(filteredGenres.slice(0, 20));
 
 var filteredData = { nodes: data["nodes"], links: filteredGenres };
-
-const stringifiedData = JSON.stringify(filteredData, null, 2);
-
+//
+//
+//
+//
+//
+//
 
 // Write to JSON
+const stringifiedData = JSON.stringify(filteredData, null, 2);
 fs.writeFile("joris/sankey.json", stringifiedData, (err) => {
   if (err) {
     throw err;
   }
-  console.log("Data successfully saved to JSON.");
+  console.log("\n\n--> Data successfully saved to JSON.");
 });
