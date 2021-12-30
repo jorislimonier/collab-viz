ac3e241c5f4b753c0e6420a27d79c1a0456c5c38
jorislimonier <joris.limonier@hotmail.fr>
2021-12-30 12:43:15 +0100

    make export work and first basic fake filtering seems to work

--- joris/sankey-data.js
+++ joris/sankey-data.js
@@ -1,60 +1,15 @@
-/*
- *Write to JSON
- */
-function writeJSON() {
-  const stringifiedData = JSON.stringify(filteredData, null, 2);
-  fs.writeFile("joris/sankey.json", stringifiedData, (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log("\n\n--> Data successfully saved to JSON.");
-  });
-}
-//
-//
-//
-//
-//
-
 // Add element to select dropdown
 var select = document.getElementById("genreSelect");
 var option = document.createElement("option");
-option.value = 3;
-option.innerHTML = 4;
+option.value = 3; // dummy values to test, remove later
+option.innerHTML = 42; // dummy values to test, remove later
 select.appendChild(option);
 
-const data = fetch("./sankey-genre.json")
-.then((response) => response.json())
-.then((data) => console.log(data));
-const fs = require("fs");
-const nodes = data["nodes"];
-const links = data["links"];
+var data = fetch("./sankey-genre.json")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
-var genres = ["Acid Rock", "Acid Jazz", "Acid House", "Acid Techno", "Pop"];
 
-const nodesToFilter = nodes
-  .filter(({ name }) => name.includes("album"))
-  .map((nodeGroup) => nodeGroup.node);
-
-console.log(nodesToFilter);
-
-var filteredGenres = links;
-// var filteredGenres = filteredGenres.filter(({ genre }) => genres.includes(genre));
-
-var filteredGenres = links.slice(0, 99).map((row) => {
-  if (
-    (nodesToFilter.includes(row.source) ||
-      nodesToFilter.includes(row.target)) &&
-    !genres.includes(row.genre)
-  ) {
-    row.value = 0;
-  }
-  delete row.genre;
-  return row;
-});
-
-console.log(filteredGenres.slice(0, 20));
-
-var filteredData = { nodes: data["nodes"], links: filteredGenres };
-
-writeJSON();
+export function dummyFunc(){
+  console.log("EXPORT WORKED")
+}
